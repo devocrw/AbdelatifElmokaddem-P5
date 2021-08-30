0f6660f35104ba23d4f856489510bccae03fd14b
devocrw <devocrw@gmail.com>
2021-08-30 16:48:11 +0200

    Suite fonction

--- panier.js
+++ panier.js
@@ -4,4 +4,17 @@ let enregistementDansLocalStorage = JSON.parse(localStorage.getItem("Articles"))
 
 let articlePanier = document.getElementById("article_panier");
 let totalPrix = document.getElementById("total_prix");
-
+const afficherProduitPanier = () => {
+    if(enregistementDansLocalStorage === null) {
+        //Si le panier est vide
+        const divPanierVide = document.createElement("div");
+        divPanierVide.className = "col text-center mx-4 h4 border px-3 py-3";
+        divPanierVide.innerHTML = "Mon panier est vide";
+        articlePanier.appendChild(divPanierVide);
+    } else {
+        //Si le panier n'est pas vide
+        for(let i = 0; i < enregistementDansLocalStorage.length; i++) {
+            let divInfo = document.createElement("div");
+            divInfo.className = "border col-lg-4 rounded-0 mb-5 px-2 align-self-stretch";
+            articlePanier.appendChild(divInfo);
+        }
