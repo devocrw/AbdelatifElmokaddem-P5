babbc491fb09520bd4ee1590c9f79b46beb88807
devocrw <devocrw@gmail.com>
2021-08-30 15:59:42 +0200

    Construction des fonctions
    panier

---
+++ panier.js
@@ -0,0 +1,7 @@
+let enregistementDansLocalStorage = JSON.parse(localStorage.getItem("Articles"));
+
+// AFFICHER LES PRODUITS DU PANIER DANS LA PAGE
+
+let articlePanier = document.getElementById("article_panier");
+let totalPrix = document.getElementById("total_prix");
+
