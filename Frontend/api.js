58dd9b4160e264bf3efd22ef4a68f3317ad81ac6
devocrw <devocrw@gmail.com>
2021-09-04 12:12:42 +0200

    Ajout commentaire
    plan test

--- Frontend/api.js
+++ Frontend/api.js
@@ -1,11 +1,14 @@
-//////// REQUETE FETCH - RECUPERATION DES ARTICLES SUR SERVEUR
+//////// REQUETE FETCH - RECUPERATION DES ARTICLES SUR SERVEUR (*plan test)
 
-const getCamera = async () => {
-    let response = await fetch("http://localhost:3000/api/cameras")
-  
-    if (response.ok) {
-      return  await response.json();
-      } else {
-        alert("erreur");
-      }
-  };  
+const getCameras = async () => {
+  let response = await fetch("http://localhost:3000/api/cameras");
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    alert("erreur");
+  }
+};
+
+getCameras();
+console.log(getCameras());
